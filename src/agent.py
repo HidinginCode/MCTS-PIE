97d7575f3ee3b34935f5a280dfb2c507bf990a2d
Malte F. Speidel <malte.speidel@speidelm.de>
2025-09-24 11:09:39 +0200

    Added agent class and tests for agent class

---
+++ src/agent.py
@@ -0,0 +1,82 @@
+"""This module holds the agent class, which collects metrics for the experiments."""
+
+from copy import deepcopy
+
+class Agent():
+    """This class represents the agent and collects metrics for the experiments.
+    """
+
+    # Metric Variables
+    step_count = 0
+    amount_of_shifts = 0
+    energy_consumption = 0.0
+    identificator = 111111111
+    path = []
+
+    def __init__(self):
+        """Init function for the Agent class."""
+        self.identificator = id(self)
+        self.energy_consumption = 0.0
+
+    def __str__(self) -> str:
+        """String function for the agent.
+
+        Returns:
+            str: String containing all metrics and identifier
+        """
+        return (
+            f"Stepcount: {self.step_count}\n"
+            f"Amount of shifts: {self.amount_of_shifts}\n"
+            f"Energy Consumption: {self.energy_consumption}\n"
+            f"Identificator: {self.identificator}"
+        )
+
+    def get_step_count(self) -> int:
+        """Returns an agents step count.
+
+        Returns:
+            int: Step count of the agent.
+        """
+        return self.step_count
+
+    def get_amount_of_shifts(self) -> int:
+        """Returns amount of shifts and agent performed.
+
+        Returns:
+            int: Amount of shifts performed by the agent.
+        """
+        return self.amount_of_shifts
+
+    def get_energy_consumption(self) -> float:
+        """Returns amount of energy used by the agent.
+
+        Returns:
+            float: Amount of energy used by the agent.
+        """
+        return self.energy_consumption
+
+    def get_idenificator(self) -> int:
+        """Returns identificator of the agent.
+
+        Returns:
+            int: Agent identificator
+        """
+        return self.identificator
+
+    def get_path(self) -> list:
+        """Returns path of the agent.
+
+        Returns:
+            list: Agent path.
+        """
+
+        return self.path
+
+    def set_path(self, path: list) -> None:
+        """Sets an agents path.
+
+        Args:
+            path (list): New path for agent
+        """
+
+        self.path = deepcopy(path)
--- src/test.py
+++ src/test.py
@@ -4,6 +4,7 @@ This module contains all the tests needed for this project.
 """
 
 from map import Map
+from agent import Agent
 
 def map_creation_test():
     """Tests if map creation works as intended.
@@ -12,20 +13,49 @@ def map_creation_test():
 
     assert len(test_map.map) == 5, (
         f"Map dimension 1 has length {len(test_map.map)},but should have 5!"
-        )
+    )
 
     for row in test_map.map:
         assert len(row) == 5, (
             f"Map dimension 2 row {row} has length {len(row)}, but should have 5"
             )
 
-    print(test_map.map)
     print("Map creation test passed.")
+
+def agent_creation_test():
+    """This function tests the creation of an Agent
+    """
+    new_agent = Agent()
+    assert isinstance(new_agent.get_idenificator(), int), (
+        "Agent get_identificator method did not return int."
+    )
+
+    assert isinstance(new_agent.get_amount_of_shifts(), int), (
+        "Agents get_amount_of_shifts method did not return int."
+    )
+
+    assert isinstance(new_agent.get_energy_consumption(), float), (
+        "Agents get_energy_consumption method did not return float."
+    )
+
+    assert isinstance(new_agent.get_path(), list), (
+        "Agents get_path method did not return list."
+    )
+
+    new_agent.set_path([[0,0], [1,1]])
+
+    assert new_agent.get_path() == [[0,0], [1,1]], (
+        f"""After setting new path, agents get_path method should have returned
+        {[[0,0], [1,1]]}, but returned {new_agent.get_path()}."""
+    )
+
+    print("Agent creation test passed.")
 
 def main():
     """Main function that runs all the defined test methods.
     """
     map_creation_test()
+    agent_creation_test()
 
 if __name__ == "__main__":
     main()
