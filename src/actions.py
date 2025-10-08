c88cf6cebab1a717c15a00850fd368414b8642f9
Malte Speidel <malte.speidel@speidelm.de>
2025-10-08 14:45:49 +0200

    Added action enum

---
+++ src/actions.py
@@ -0,0 +1,10 @@
+"""This module contains the action enum that holds all possible actions.
+"""
+
+from enum import Enum
+
+class Actions(Enum):
+    """Action enum that contains all possible actions."""
+    NOTHING = 0
+    PICKUP = 1
+    DROPOFF = 2
